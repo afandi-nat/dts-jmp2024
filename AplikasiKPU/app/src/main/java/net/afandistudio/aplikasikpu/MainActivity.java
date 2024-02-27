c7c2c65fb72e15d06c86ce32d49ba3637f5b3bfa
afandi-nat <afandi@polines.ac.id>
2024-02-27 15:50:13 +0700

    delete aplikasi kpu

--- AplikasiKPU/app/src/main/java/net/afandistudio/aplikasikpu/MainActivity.java
+++ AplikasiKPU/app/src/main/java/net/afandistudio/aplikasikpu/MainActivity.java
@@ -2,13 +2,27 @@ package net.afandistudio.aplikasikpu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEntry = findViewById(R.id.btn_entry);
+        btnEntry.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_entry){
+            Intent entryIntent = new Intent(MainActivity.this, EntryFormActivity.class);
+            startActivity(entryIntent);
+        }
     }
 }
